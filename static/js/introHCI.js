cea5e12632f69ac3c40bfaaef94ca17f82344a1e
Yuvaraj Kakaraparthi <ykakarap@eng.ucsd.edu>
2020-01-16 10:46:10 -0800

    added JS

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -4,6 +4,15 @@ $(document).ready(function() {
 
 function initializePage() {
     $('#testjs').click(function(e) {
-            $('.jumbotron h1').text("Javascript is connected");
-            });
+        $('.jumbotron h1').text("Javascript is connected");
+    });
+
+
+    $("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e) {
+    e.preventDefault();
+    
+    $(this).css("background-color", "#7fff00");
 }
